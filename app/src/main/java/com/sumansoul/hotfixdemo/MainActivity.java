39ce3b6883649a0b97b8565123caf4613c3b2d65
luo3672 <1138191036@qq.com>
2020-07-24 18:15:51 +0800

    Initial commit

---
+++ app/src/main/java/com/sumansoul/hotfixdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.sumansoul.hotfixdemo;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i = 1/0;
+            }
+        });
+
+    }
+
+
+}
